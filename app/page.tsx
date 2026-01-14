44c7956d8d417183c6c919fea74df676089bdfcd
ukasha <ukashamughal3.0@gmail.com>
2026-01-15 03:44:27 +0500

    added pre loading animation

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ import Preloader from "./components/Preloader";
 export default function Home() {
   return (
     <>
-      {/* <Preloader /> */}
+      <Preloader />
       <main className="w-full bg-white text-white">
         <section className="relative h-full w-full overflow-hidden">
           <div className="relative z-10 max-w-[1140px] mx-auto mt-30 mb-30 flex flex-col items-center max-sm:p-6 text-center">
