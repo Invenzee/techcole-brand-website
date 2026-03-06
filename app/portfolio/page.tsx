a9803a059eeed664568c1a46e670b62f42e0acc8
ukasha <ukashamughal3.0@gmail.com>
2026-03-07 02:14:15 +0500

    removed the duplicated header in portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,7 +4,6 @@ import PortfolioStack from '../components/portfolio/PortfolioStack'
 export default function page() {
     return (
         <main className="bg-white">
-            <Header />
             <PortfolioStack />
         </main>
     )
